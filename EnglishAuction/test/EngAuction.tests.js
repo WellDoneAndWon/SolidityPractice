a2b4894e7c9f2903dfae5db135e2ae3579502ea6
WellDoneAndWon <ildanaxy@gmail.com>
2023-02-07 23:40:14 +0500

    WellDone

--- EnglishAuction/test/EngAuction.tests.js
+++ EnglishAuction/test/EngAuction.tests.js
@@ -1,20 +1,20 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-import type { EngAuction } from "../typechain-types";
 
 describe("EngAuction", async function(){
-    let owner
-    let bid1
-    let bid2
+    let ownerSeller
+    let buyer1
+    let buyer2
     const itenName = "huawei"
     const minPrice = 10000000000000000
     const enoughPrice = 5000000000000000000
 
     beforeEach("EngAuction", async function(){
-        [owner, bid1, bid2] = await ethers.getSigners()
-        const EngAuction = await ethers.getContractFactory("EngAuction", owner)
-        const auction: EngAuction = await EngAuction.deploy(itenName, minPrice, enoughPrice)
+        [ownerSeller, buyer1, buyer2] = await ethers.getSigners()
+        const EngAuction = await ethers.getContractFactory("EngAuction", ownerSeller)
+        const auction = await EngAuction.deploy(itenName, minPrice, enoughPrice)
         await auction.deployed()
+
     });
 
     it("should be deployed", async function(){
